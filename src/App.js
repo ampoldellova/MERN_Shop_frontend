b3fe203f6d810dffe41d756cc75ad959f5dbd43b
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-10-12 11:26:01 +0800

    Product Details

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './Components/Layouts/Header'
 import Footer from './Components/Layouts/Footer'
 import Home from './Components/Home'
+import ProductDetails from './Components/Product/ProductDetails.js'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} exact="true" />
+          <Route path="/product/:id" element={<ProductDetails />} exact="true" />
         </Routes>
         <Footer />
       </Router>
