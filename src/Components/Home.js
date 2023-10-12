0d3538f5a910c026dc20f2571832bae2bdaefe2b
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-10-12 10:30:49 +0800

    Home Page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,8 @@
-import React, { Fragment, useState } from 'react'
-import MetaData from './Layouts/Metadata'
+import React, { Fragment, useState, useEffect } from 'react'
+import MetaData from './Layout/Metadata'
 import axios from 'axios';
+
+import Product from './Product/Product';
 
 const Home = () => {
     const [loading, setLoading] = useState(false)
@@ -15,6 +17,11 @@ const Home = () => {
         console.log(res)
         setProducts(res.data.products)
     }
+
+    useEffect(() => {
+        getProducts()
+    }, [])
+    console.log(products)
     return (
         <Fragment>
             <MetaData title={'Buy Best Products Online'} />
@@ -22,27 +29,9 @@ const Home = () => {
                 <h1 id="products_heading">Latest Products</h1>
                 <section id="products" className="container mt-5">
                     <div className="row">
-                        <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-                            <div className="card p-3 rounded">
-                                <img
-                                    className="card-img-top mx-auto"
-                                    src="https://m.media-amazon.com/images/I/617NtexaW2L._AC_UY218_.jpg"
-                                />
-                                <div className="card-body d-flex flex-column">
-                                    <h5 className="card-title">
-                                        <a href="">128GB Solid Storage Memory card - SanDisk Ultra</a>
-                                    </h5>
-                                    <div className="ratings mt-auto">
-                                        <div className="rating-outer">
-                                            <div className="rating-inner"></div>
-                                        </div>
-                                        <span id="no_of_reviews">(5 Reviews)</span>
-                                    </div>
-                                    <p className="card-text">$45.67</p>
-                                    <a href="#" id="view_btn" className="btn btn-block">View Details</a>
-                                </div>
-                            </div>
-                        </div>
+                    {products && products.map(product => (
+                        <Product key={product._id} product={product} col={4} />
+                    ))}
                     </div>
                 </section>
             </div>
