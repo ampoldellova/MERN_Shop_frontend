4b0d2f24f39b2878c8f042a03c7deeb0e4dc8402
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-10-17 11:33:25 +0800

    Register user

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './Components/Layouts/Footer'
 import Home from './Components/Home'
 import Login from './Components/User/Login'
 import ProductDetails from './Components/Product/ProductDetails.js'
+import Register from './Components/User/Register'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/product/:id" element={<ProductDetails />} exact="true" />
           <Route path="/search/:keyword" element={<Home />} exact="true" />
           <Route path="/login" element={<Login />} exact="true"/>
+          <Route path="/register" element={<Register />} exact="true" />
         </Routes>
         <Footer />
       </Router>
