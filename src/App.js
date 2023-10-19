5c0a83aa9a5dd8cca5cf898ba75c8be81bcffeee
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-10-19 11:24:01 +0800

    token functions

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ import ProductDetails from './Components/Product/ProductDetails'
 import Login from './Components/User/Login'
 import Register from './Components/User/Register';
 import Profile from './Components/User/Profile'
-
-
-
+import UpdateProfile from './Components/User/UpdateProfile';
 
 function App() {
   return (
@@ -20,10 +18,10 @@ function App() {
           <Route path="/" element={<Home />} exact="true" />
           <Route path="/product/:id" element={<ProductDetails />} exact="true" />
           <Route path="/search/:keyword" element={<Home />} exact="true" />
-
           <Route path="/login" element={<Login />} exact="true"/>
           <Route path="/register" element={<Register />} exact="true" />
           <Route path="/me" element={<Profile />} exact="true" />
+          <Route path="/me/update" element={<UpdateProfile />} exact="true"/>
         </Routes>
         <Footer />
       </Router>
